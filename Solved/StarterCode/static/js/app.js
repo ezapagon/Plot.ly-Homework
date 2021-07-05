a0a4e5335e142fddf4c597d4a4a96ed530cfcd38
ezapagon <ezapagon@gmail.com>
2021-07-04 22:44:33 -0500

    Changes

--- Solved/StarterCode/static/js/app.js
+++ Solved/StarterCode/static/js/app.js
@@ -1,30 +1,51 @@
 
-const path = "/data/samples.json"
-
-const dataPromise = d3.json(path);
-
-console.log(dataPromise);
-
-
-
-
-//function init() {
-    // Select the dropdown element
-    // var selector = d3.select("#selDataset");
+  
+ function Metadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata= data.metadata;
+    var Endarray= metadata.filter(sampleobject => sampleobject.id == sample);
+    var End = Endarray[0]
+    var Index = d3.select("#sample-metadata");
     
-    // Populate the dropdown with subject ID's from the list of sample Names
-      // d3.json("/data/samples.json").then((data) => {
-        // var subjectIds = data.names;
-        // subjectIds.forEach((id) => {
-          // selector
-          // .append("option")
-          // .text(id)
-          // .property("value", id);
-        // });
-      
-      // Use the first subject ID from the names to build initial plots
-      // const firstSubject = subjectIds[0];
-      // updateCharts(firstSubject);
-      // updateMetadata(firstSubject);
-    // });
-  //}
+    Index.html("");
+    Object.entries(End).forEach(([key, value]) => {
+
+      Index.append("h6").text(`${key}: ${value}`);
+    });
+  });
+}
+
+
+function Charts(sample) {
+
+d3.json("samples.json").then((data) => {
+  var samples= data.samples;
+  var Endarray= samples.filter(sampleobject => sampleobject.id == sample);
+  var End = Endarray[0]
+
+  var ids = End.otu_ids;
+  var labels = End.otu_labels;
+  var values = End.sample_values;
+
+  var barInfo =[
+    {
+      y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+      x:values.slice(0,10).reverse(),
+      text:labels.slice(0,10).reverse(),
+      type:"bar",
+      orientation:"h"
+
+    }
+  ];
+
+  var barTitle = {
+    title: "The top 10 OTUs found in that individual",
+    margin: { t: 50, l: 180 }
+  };
+
+  Plotly.newPlot("bar", barInfo, barTitle);
+});
+}
+
+
+
